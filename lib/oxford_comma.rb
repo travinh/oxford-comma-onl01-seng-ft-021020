89abb48d94f443d4d64d97fd5e443f15f11057b7
astronomical-reducer-9741 <vinh.v.tran@ucdenver.edu>
2020-02-13 01:20:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+  if (array.size ==1) 
+    array[0]
+  elsif (array.size==2)
+    string = array.join(" and ")
+    string
+  elsif (array.size==3)
+    array[-1] = "and " + array.last
+    string = array.join(", ")
+    string
+  elsif (array.size >3)
+    array[-1] = "and " + array.last
+    string = array.join(", ")
+    string
+  end
 end
